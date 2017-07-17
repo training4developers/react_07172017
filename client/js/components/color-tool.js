94d4c841cb273ff47227551a6b625ad48725eaa2
Eric W. Greene <eric@ericwgreene.com>
2017-07-17 14:49:43 -0400

    added colors array demo

--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -3,11 +3,17 @@ import React from 'react';
 export class ColorTool extends React.Component {
 
   render() {
+
+    const colors = ['red','green','yellow','blue'];
+
     // return React.createElement('h1', null, 'Color Tool');
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
+      <ul>
+        {colors.map(color => <li>{color}</li>)}
+      </ul>
     </div>;
   }
 }
